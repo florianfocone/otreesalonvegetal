e2ae17629f213e3686538d049fd18637fa70d20c
USERLAB-011 <focone.florian@gmail.com>
2022-09-13 09:50:12 +0200

    order

--- TestFF/__init__.py
+++ TestFF/__init__.py
@@ -364,7 +364,10 @@ def set_payoffs(group):
         player.contrib3 = players[2].choix_pot_commun
         player.contrib4 = players[3].choix_pot_commun
 
-
+def creating_session(subsession):
+    labels = ['1', '2', '3','4', '5', '6','7', '8', '9','10','11', '12', '13','14', '15', '16','17', '18', '19','20','21', '22', '23','24', '25', '26','27', '28', '29','30','31', '32', '33','34', '35', '36','37', '38', '39','40']
+    for player, label in zip(subsession.get_players(), labels):
+        p.participant.label = label
 
 
 # PAGES
